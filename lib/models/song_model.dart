bf25a2da477084061c9fb8832c821dcd4782fd3d
Diego <diegochiari86@gmail.com>
2024-06-10 14:30:40 -0300

    modelo song con premium

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -2,9 +2,13 @@ class SongModel {
   String name;
   String bgImage;
   String duration;
+  bool isPremium;
 
   SongModel(
-      {required this.name, required this.bgImage, required this.duration});
+      {required this.name,
+      required this.bgImage,
+      required this.duration,
+      required this.isPremium});
 
   static List<SongModel> getDescansarMejorSongs() {
     List<SongModel> songs = [];
@@ -12,10 +16,14 @@ class SongModel {
     songs.add(SongModel(
         name: 'En el bosque',
         bgImage: 'assets/bgs/zen1.jpeg',
-        duration: '30 mins'));
+        duration: '30 mins',
+        isPremium: false));
 
     songs.add(SongModel(
-        name: 'Lluvia', bgImage: 'assets/bgs/zen1.jpeg', duration: '20 mins'));
+        name: 'Lluvia',
+        bgImage: 'assets/bgs/zen1.jpeg',
+        duration: '20 mins',
+        isPremium: false));
 
     return songs;
   }
@@ -26,17 +34,20 @@ class SongModel {
     songs.add(SongModel(
         name: 'Meditacion 1',
         bgImage: 'assets/bgs/zen2.jpeg',
-        duration: '4 mins'));
+        duration: '4 mins',
+        isPremium: false));
 
     songs.add(SongModel(
         name: 'Meditacion 2',
         bgImage: 'assets/bgs/zen2.jpeg',
-        duration: '5 mins'));
+        duration: '5 mins',
+        isPremium: false));
 
     songs.add(SongModel(
         name: 'Meditacion 3',
         bgImage: 'assets/bgs/zen2.jpeg',
-        duration: '15 mins'));
+        duration: '15 mins',
+        isPremium: true));
 
     return songs;
   }
